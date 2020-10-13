71bb1fc2aa69ac338fa3956cf7e48d55defcd50d
Melanie Hall <hallmt313@gmail.com>
2020-10-13 16:29:56 -0400

    Portfolio page fleshed out

--- assets/project.js
+++ assets/project.js
@@ -1,10 +1,33 @@
 $(document).ready(function(){
-
+    // Navbar Links
     $(".fa-github").click(function(){
         window.open("https://github.com/mhall313")
     })
     $(".fa-linkedin").click(function(){
         window.open("https://www.linkedin.com/in/hallmelanie/")
+    })
+    // BandMates Links
+    $(".bandmates-app").click(function(){
+        window.open("https://joshallan.dev/bandmates-musician-finder/")
+    })
+    $(".bandmates-repo").click(function(){
+        window.open("https://github.com/jallan07/bandmates-musician-finder")
+    })
+
+    // Do I Need a Jacket Links
+    $(".weather-dapp").click(function(){
+        window.open("https://mhall313.github.io/weather-db/")
+    })
+    $(".weather-repo").click(function(){
+        window.open("https://github.com/mhall313/weather-db")
+    })
+
+    // Coding Quiz Links
+    $(".weather-dapp").click(function(){
+        window.open("https://mhall313.github.io/code-quiz/")
+    })
+    $(".weather-repo").click(function(){
+        window.open("https://github.com/mhall313/code-quiz")
     })
     
     }) //end of doc
